e4350ab68f7659b979b869eb91d85ce4c7bf2071
lly <1776321381@qq.com>
2025-11-18 14:37:15 +0800

    feat: 整数反转

--- leetcode/editor/cn/reverse-integer.js
+++ leetcode/editor/cn/reverse-integer.js
@@ -11,11 +11,27 @@
  * @return {number}
  */
 var reverse = function(x) {
-    
+    const MIN = -Math.pow(2, 31)
+    const MAX = Math.pow(2, 31) - 1
+    if (x === 0) {
+      return 0
+    }
+    const isFlag = x < 0
+    const absX = Math.abs(x)
+    const num = absX.toString().split('').reverse().join('')
+    const result = isFlag ? -Number(num) : Number(num)
+    if (result > MAX || result < MIN) {
+      return 0
+    } else {
+      return result
+    }
 };
 // @lc code=end
 
-
+console.log(reverse(123))
+console.log(reverse(-123))
+console.log(reverse(0))
+console.log(reverse(120))
 
 /*
 // @lcpr case=start
